8b5d20c237ee0b54460c29d36e5a490cb57283a8
gugacarbo <gustavo@digitalscudero.com.br>
2022-12-09 18:14:25 -0300

    Home Responsive

--- src/Index/Screens/Index/components/HomeButton/Styled.jsx
+++ src/Index/Screens/Index/components/HomeButton/Styled.jsx
@@ -1,23 +1,14 @@
 import styled from "styled-components";
-
-export const Button = styled.button`
+import { BaseButton } from "../../../../Styled";
+export const Button = styled(BaseButton)`
   grid-area: HomeButton;
-  border: none;
-  border: 1px solid ${({ theme }) => theme.color.main.color};
-  font-weight: bold;
+  border-color: ${({ theme }) => theme.color.main.color};
   background-color: ${({ theme }) => theme.background};
   color: ${({ theme }) => theme.color.main.color};
-  padding: 0.3rem 0;
-  font-size: 1.1rem;
-  border-radius: 30px;
-  font-weight: 300;
-  text-decoration: none;
-  transition: ${({ theme }) => theme.transition.main};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
+
+  margin-left: 0;
   margin-right: auto;
+
   &:hover {
     background-color: ${({ theme }) => theme.color.main.color};
     color: ${({ theme }) => theme.color.white};
@@ -25,16 +16,11 @@ export const Button = styled.button`
   }
   @media (max-width: 768px) {
     margin: 0 auto;
-    font-size: 1.6rem;
     font-weight: 500;
-
   }
   @media (max-width: 520px) {
     margin: 0 auto;
-    font-size: 1.6rem;
-    width: 85%;
   }
   @media (max-width: 300px) {
-    width: 100%;
   }
 `;
